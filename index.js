7779d0c6b88f3a981ad42b74a24d0eccbe4fe990
Richard Walker <digitalsadhu@gmail.com>
2016-09-01 15:02:52 +0200

    Initial implementation via regex matching

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
+'use strict'
 
+const _ = require('lodash')
+
+// TODO: allow comment syntax to allow the user to override the relationship definition for a model
+
+const RELATIONSHIP_TYPES = Object.freeze({
+  BELONGS_TO: 'belongsTo',
+  BELONGS_TO_MANY: 'belongsToMany',
+  HAS_ONE: 'hasOne',
+  HAS_MANY: 'hasMany',
+  MORPH_ONE: 'morphOne',
+  MORPH_TO: 'morphTo',
+  MORPH_MANY: 'morphMany'
+})
+
+const MATCHERS = Object.freeze({
+  INNER_FUNCTION_BLOCK: /\)\s*{([\s\S]*)}/,
+  RELATIONSHIP_TYPE: /return\s*this\s*\.\s*([^\(]*)\s*\(/,
+  THROUGH: /\.*through\s*\(/,
+  MODEL_TO_NAME: new RegExp(`(?:${_.values(RELATIONSHIP_TYPES).join('|')})\\s*\\(([^,\\)]*)`),
+  THROUGH_MODEL_NAME: new RegExp(`\\s*through\\s*\\(([^,\\)]*)`)
+})
+
+module.exports = (modelName, Model) => {
+  const instance = new Model()
+  const proto = Reflect.getPrototypeOf(instance)
+  const relationships = {}
+  Reflect.ownKeys(proto).forEach(prop => {
+    if (['constructor', 'tableName', 'idAttribute'].indexOf(prop) !== -1) return
+
+    const relationName = prop
+    const functionString = proto[relationName].toString()
+    const innerBlock = functionString.match(MATCHERS.INNER_FUNCTION_BLOCK)
+
+    if (!innerBlock) return
+
+    const capture = (str, matcher) => matcher.test(str) ? str.match(matcher)[1] : null
+
+    relationships[prop] = {
+      name: prop,
+      type: capture(functionString, MATCHERS.RELATIONSHIP_TYPE),
+      through: MATCHERS.THROUGH.test(functionString),
+      keyFrom: null,
+      keyTo: null,
+      modelFromName: modelName,
+      modelToName: capture(functionString, MATCHERS.MODEL_TO_NAME),
+      throughModelName: capture(functionString, MATCHERS.THROUGH_MODEL_NAME)
+    }
+
+    if (RELATIONSHIP_TYPES.BELONGS_TO === relationships[prop].type) {
+      relationships[prop].keyFrom = _.snakeCase(`${relationships[prop].modelToName}_id`)
+    } else {
+      relationships[prop].keyTo = _.snakeCase(`${relationships[prop].modelFromName}_id`)
+    }
+  })
+  return relationships
+}
--- test/default.spec.js
+++ test/default.spec.js
@@ -1,7 +1,111 @@
 /* global describe, it */
-var assert = require('assert')
-describe('the module', function () {
-  it('should should be ok', function () {
-    assert.ok(true)
+'use strict'
+
+const assert = require('assert')
+const knex = require('knex')
+const bookshelf = require('bookshelf')(knex({}))
+const relations = require('..')
+
+const Post = bookshelf.Model.extend({
+  tableName: 'posts',
+
+  user () {
+    return this.belongsTo(User, ['userid'])
+  },
+
+  comments () {
+    return this.hasMany(Comment)
+  }
+})
+
+const Comment = bookshelf.Model.extend({
+  tableName: 'comments',
+
+  post () {
+    return this.belongsTo(Post)
+  }
+})
+
+const User = bookshelf.Model.extend({
+  tableName: 'users',
+
+  posts () {
+    return this.hasMany(Post)
+  },
+
+  comments () {
+    return this.hasMany(Comment)
+  }
+})
+
+const Book = bookshelf.Model.extend({
+  tableName: 'books',
+
+  paragraphs: function () {
+    return this.hasMany(Paragraph).through(Chapter)
+  },
+
+  chapters: function () {
+    return this.hasMany(Chapter)
+  }
+})
+
+let Chapter = bookshelf.Model.extend({
+  tableName: 'chapters',
+
+  paragraphs: function () {
+    return this.hasMany(Paragraph)
+  }
+})
+
+let Paragraph = bookshelf.Model.extend({
+  tableName: 'paragraphs',
+
+  book: function () {
+    return this.belongsTo(Book).through(Chapter)
+  }
+})
+
+describe('bookshelf-model-relations', () => {
+  it('relation defn for Post model', () => {
+    const relationDefn = relations('Post', Post)
+    assert.deepEqual(relationDefn, {
+      user: {
+        name: 'user',
+        type: 'belongsTo',
+        keyFrom: 'user_id',
+        modelFromName: 'Post',
+        keyTo: null,
+        modelToName: 'User',
+        through: false,
+        throughModelName: null
+      },
+      comments: {
+        name: 'comments',
+        type: 'hasMany',
+        keyFrom: null,
+        modelFromName: 'Post',
+        keyTo: 'post_id',
+        modelToName: 'Comment',
+        through: false,
+        throughModelName: null
+      }
+    })
+  })
+
+  it('through', () => {
+    const relationDefn = relations('Paragraph', Paragraph)
+    assert.deepEqual(relationDefn, {
+      book: {
+        name: 'book',
+        type: 'belongsTo',
+        keyFrom: 'book_id',
+        modelFromName: 'Paragraph',
+        keyTo: null,
+        modelToName: 'Book',
+        through: true,
+        throughModelName: 'Chapter'
+      }
+    })
   })
 })
