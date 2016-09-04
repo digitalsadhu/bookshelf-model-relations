2ca087c2c251711395e536254015c33ac028779b
Richard Walker <digitalsadhu@gmail.com>
2016-09-04 21:19:30 +0200

    Refactor tests

--- test/default.spec.js
+++ test/default.spec.js
@@ -6,76 +6,26 @@ const knex = require('knex')
 const bookshelf = require('bookshelf')(knex({}))
 const relations = require('..')
 
-const Post = bookshelf.Model.extend({
-  tableName: 'posts',
-  idAttribute: '_id',
-
-  user () {
-    return this
-      .belongsTo(User, ['userid'])
-  },
-
-  comments () {
-    return this.hasMany(Comment)
-  },
-
-  other () {
-    console.log('is not a relationship function')
-  }
-})
-
-const Comment = bookshelf.Model.extend({
-  tableName: 'comments',
-
-  post () {
-    return this.belongsTo(Post)
-  }
-})
-
-const User = bookshelf.Model.extend({
-  tableName: 'users',
-
-  posts () {
-    return this.hasMany(Post)
-  },
-
-  comments () {
-    return this.hasMany('Comment')
-  }
-})
-
-const Book = bookshelf.Model.extend({
-  tableName: 'books',
-
-  paragraphs: function () {
-    return this.hasMany(Paragraph).through(Chapter)
-  },
-
-  chapters: function () {
-    return this.hasMany(Chapter)
-  }
-})
-
-let Chapter = bookshelf.Model.extend({
-  tableName: 'chapters',
-
-  paragraphs: function () {
-    return this.hasMany(Paragraph)
-  }
-})
-
-let Paragraph = bookshelf.Model.extend({
-  tableName: 'paragraphs',
-
-  book: function () {
-    return this
-      .belongsTo(Book)
-      .through('Chapter')
-  }
-})
-
 describe('bookshelf-model-relations', () => {
-  it('relation defn for Post model', () => {
+  it('general test of relation output', () => {
+    const Post = bookshelf.Model.extend({
+      tableName: 'posts',
+      idAttribute: '_id',
+      user () { return this.belongsTo(User, ['userid']) },
+      comments () { return this.hasMany(Comment) },
+      other () { console.log('is not a relationship function') }
+    })
+
+    const Comment = bookshelf.Model.extend({
+      tableName: 'comments',
+      post () { return this.belongsTo(Post) }
+    })
+
+    const User = bookshelf.Model.extend({
+      tableName: 'users',
+      posts () { return this.hasMany(Post) },
+      comments () { return this.hasMany('Comment') }
+    })
     const relationDefn = relations(Post)
     assert.deepEqual(relationDefn, {
       user: {
@@ -323,11 +273,19 @@ describe('bookshelf-model-relations', () => {
   })
 
   it('modelName option', () => {
+    let Paragraph = bookshelf.Model.extend({
+      tableName: 'paragraphs',
+      book: function () { return this.belongsTo('Book') }
+    })
     const relationDefn = relations(Paragraph, {modelName: 'other'})
     assert.deepEqual(relationDefn.book.modelFrom, 'other')
   })
 
   it('support registry syntax', () => {
+    const User = bookshelf.Model.extend({
+      tableName: 'users',
+      comments () { return this.hasMany('Comment') }
+    })
     const relationDefn = relations(User)
     assert.equal(relationDefn.comments.modelTo, 'Comment')
   })
